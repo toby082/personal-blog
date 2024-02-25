4d69411ad500407066ee910de3a3f577c427119e
Jiang Jiawen <jianwen.jiang@sembcorp.com>
2024-02-24 22:51:02 -0500

    first commit

---
+++ lib/social-data.ts
@@ -0,0 +1,36 @@
+import { SocialProfile } from "@/types";
+
+export const socialProfiles: SocialProfile[] = [
+  {
+    name: "github",
+    link: "https://github.com/toby082",
+  },
+  {
+    name: "instagram",
+    link: "https://instagram.com/toby082",
+  },
+  {
+    name: "linkedin",
+    link: "https://linkedin.com/in/toby082",
+  },
+  {
+    name: "mastodon",
+    link: "https://mastodon.social/@toby082",
+  },
+  {
+    name: "threads",
+    link: "https://threads.net/@toby082",
+  },
+  {
+    name: "tiktok",
+    link: "https://tiktok.com/@toby082",
+  },
+  {
+    name: "x",
+    link: "https://twitter.com/toby082",
+  },
+  {
+    name: "youtube",
+    link: "https://youtube.com/@toby082",
+  },
+];
